9a3818a8897273015730cc07cbf45d772b19781b
Serhii Diachok <sergey@sdyachok.com.ua>
2020-03-21 11:19:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Advanced Notes Application
+
+Setup:
+```bash
+# create virtual env
+python -m venv .venv
+
+# setup requirements
+pip install -r .\requirements.txt
+```
---
+++ notes.py
@@ -0,0 +1,30 @@
+import os
+import time
+import json
+
+
+class NotesApp:
+    def __init__(self, filename=None):
+        self.filename = filename if filename is not None else "notes.json"
+
+    def init(self):
+        if not os.path.exists(self.filename):
+            with open(self.filename, "w") as f:
+                json.dump({"notes": []}, f, indent=2, sort_keys=True)
+
+    def run(self):
+        while True:
+            print("I'm running")
+            time.sleep(1)
+    
+    def shutdown(self):
+        print("Shutdown")
+
+
+if __name__ == "__main__":
+    app = NotesApp()
+    try:
+        app.init()
+        app.run()
+    except KeyboardInterrupt:
+        app.shutdown()
